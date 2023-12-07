30de7d13ce2b28e2700480de0d711404d0edbd94
Sam L. Davis, Ph.D <ecology-rocks@users.noreply.github.com>
2023-12-07 10:30:19 -0500

    updating to daily build

---
+++ src/utils/getPost.ts
@@ -0,0 +1,23 @@
+import { getCollection } from "astro:content";
+import { isBefore, parseISO } from "date-fns";
+
+export const getPostsCC = async (props = {}: { limit?: number } = {}) => {
+  let allPosts = await getCollection("posts");
+
+  if (!import.meta.env.DEV) {
+    allPosts = allPosts
+      .filter((p) => isBefore(parseISO(p.data.pubDate.valueOf()), new Date()))
+      .filter((p) => p.data.status.toLowerCase() === "published");
+  }
+
+  const posts = allPosts.sort(
+    (a, b) =>
+      new Date(b.data.pubDate.valueOf()) - new Date(a.data.pubDate.valueOf())
+  );
+
+  if (props?.limit) return posts.slice(0, props.limit);
+
+  return posts;
+};
+
+// from https://www.curiouslychase.com/posts/how-i-schedule-blog-posts-with-github-actions-and-netlify/
